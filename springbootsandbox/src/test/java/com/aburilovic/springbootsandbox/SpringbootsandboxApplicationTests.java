46f28a28e882c7baa1ae2c91e5942ce2d20fbb44
Ante Burilovic <ante.burilovic.dev@gmail.com>
2022-10-11 14:12:58 +0200

    Initial commit

---
+++ springbootsandbox/src/test/java/com/aburilovic/springbootsandbox/SpringbootsandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aburilovic.springbootsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
